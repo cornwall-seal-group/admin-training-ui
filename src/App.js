fd27a63f9424e792354d379be46dd7e6f0b2b271
Richard Frost <richard.frost@headforwards.com>
2019-05-30 13:09:20 +0100

    Updated app to use HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, HashRouter as Router } from "react-router-dom";
 import ImageWrapper from "./components/image-wrapper";
 import SealPicker from "./components/seal-picker";
 
