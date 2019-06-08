3151e024df1b93fb26231039e73f7a9d22a1cb39
Richard Frost <richard.frost@headforwards.com>
2019-06-08 17:53:52 +0100

    Make requests for predictions based on iteration number

--- src/components/image-comparator/seal-prediction.js
+++ src/components/image-comparator/seal-prediction.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function({ baseUrl, id, percentage, image }) {
+export default function({ baseUrl, id, percentage, image, iteration }) {
   return (
     <div class="card grey lighten-2">
       <span class="new badge" data-badge-caption="%">
@@ -9,7 +9,7 @@ export default function({ baseUrl, id, percentage, image }) {
       <img
         key={`prediction${image}`}
         style={{ "max-width": "100%" }}
-        src={`${baseUrl}/${id}/predictions/${image}`}
+        src={`${baseUrl}/${id}/${iteration}/${image}`}
         alt="Prediction img for {id}"
       />
     </div>
