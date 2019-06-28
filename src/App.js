55db8c455297fccb6f5a2480c9145ab4b4581c8b
Rich Frost <richard@ipponsolutions.co.uk>
2019-06-28 22:44:10 +0100

    Added screen to check results of pipeline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, HashRouter as Router } from "react-router-dom";
 
 import Menu from "./components/menu";
 import ProjectScreen from "./screens/project-screen";
+import ClassifierScreen from "./screens/classifier-tester";
+
 function App() {
   return (
     <>
@@ -16,6 +18,7 @@ function App() {
               <div className="col s12 m12 xl12">
                 <Router>
                   <Route path="/project/:id" component={ProjectScreen} />
+                  <Route path="/classifier/" component={ClassifierScreen} />
                 </Router>
               </div>
             </div>
