63154759f77336df8932f5f31dc97408a5b86a41
Rich Frost <richard@ipponsolutions.co.uk>
2019-06-29 16:17:22 +0100

    Added ability to pick the different seals

--- src/screens/classifier-tester.js
+++ src/screens/classifier-tester.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 import Axios from "axios";
 import { baseUrl } from "../config.json";
-import SealCard from "../components/seal-picker/seal-card";
-import SealImages from "../components/seal-images";
 
 export default class ProjectScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seals: {}
+      seals: {},
+      selectedSeal: null
     };
   }
 
@@ -29,9 +27,13 @@ export default class ProjectScreen extends Component {
     });
   }
 
+  pickSeal = seal => {
+    this.setState({ selectedSeal: seal });
+  };
+
   render() {
-    const { seals = {} } = this.state;
-    const seal = "LIZ296";
+    const { seals = {}, selectedSeal = "" } = this.state;
+    const seal = selectedSeal;
 
     const first =
       seals[
@@ -40,34 +42,48 @@ export default class ProjectScreen extends Component {
 
     return (
       <>
+        {Object.keys(seals).map(seal => {
+          const s = seal.split("/")[seal.split("/").length - 1];
+          return (
+            <button
+              className="waves-effect waves-light btn"
+              onClick={() => this.pickSeal(s)}
+            >
+              {s}
+            </button>
+          );
+        })}
         <h1>Results for {seal}</h1>
-        <div className="row">
-          {Object.keys(first).map(key => {
+        {Object.keys(first)
+          .sort((a, b) => a.split("-")[2] - b.split("-")[2])
+          .map(key => {
             return (
-              <>
-                <div className="col col-6">
+              <div className="row">
+                <div className="col s12">{key}</div>
+                <div className="col s6">
                   <img
+                    style={{ "max-width": "100%" }}
                     alt="seal"
                     src={`${baseUrl}/classifier-test-images/41e2a93c-9091-4bbd-a3b4-14be00814812/${seal}/${key}`}
                   />
                 </div>
-                <div className="col col-6">
+                <div className="col s6">
                   <ul>
                     {Object.keys(first[key])
                       .sort((a, b) => first[key][b] - first[key][a])
                       .map(prediction => {
                         return (
                           <li className={prediction === seal ? "green" : ""}>
-                            {prediction}: {first[key][prediction]}{" "}
+                            {prediction}:{" "}
+                            {(first[key][prediction] * 100).toFixed(2)}%
                           </li>
                         );
                       })}
                   </ul>
                 </div>
-              </>
+              </div>
             );
           })}
-        </div>
       </>
     );
   }
